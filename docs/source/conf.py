6195505a8e456a7a83e2795b460460ee62b86ab2
Flouttre <florent.daerden38@gmail.com>
2023-07-31 01:24:16 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Particle Toolkit'
+copyright = '2023, Flotter Studio'
+author = 'Florent Daerden (Flo_12344)'
 
 release = '0.1'
 version = '0.1.0'
